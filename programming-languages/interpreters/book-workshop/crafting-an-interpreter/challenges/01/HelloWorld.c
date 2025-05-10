e344d6af8bf79aa56e081aeeb8aaec56fb131ecd
Juan Zurita <juanzq10dev@gmail.com>
2025-05-10 14:37:53 -0400

    Add chapter 2 and 3 notes in handbook

---
+++ programming-languages/interpreters/book-workshop/crafting-an-interpreter/challenges/01/HelloWorld.c
@@ -0,0 +1,4 @@
+// TODO 
+// define a doubly-linked
+// list of heap-allocated strings. Write functions to insert, find, and delete items from
+// it. Test them.
---
+++ programming-languages/interpreters/book-workshop/crafting-an-interpreter/handbook/02/Parts of a language.mm
@@ -0,0 +1,146 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Parts of a language" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1746153073604" VGAP_QUANTITY="3 pt"><hook NAME="MapStyle" background="#d6e8e8ff" zoom="0.826">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" show_icons="BESIDE_NODES" associatedTemplateLocation="template:/light_sky_element_template.mm" show_tags="UNDER_NODES" showTagCategories="false"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_4172522" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#051552" BACKGROUND_COLOR="#5cd5e8" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.7 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#1164b0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_4172522" STARTINCLINATION="116.25 pt;0 pt;" ENDINCLINATION="116.25 pt;28.5 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#051552" WIDTH="2" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#fff024" BACKGROUND_COLOR="#000000"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10" ITALIC="true"/>
+<edge COLOR="#000000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#ffffff" BACKGROUND_COLOR="#cc7212" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#1164b0"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_1823054225" COLOR="#fff024" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#9d5e19">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#9d5e19" TRANSPARENCY="255" DESTINATION="ID_1823054225"/>
+<font SIZE="11" BOLD="true"/>
+<edge COLOR="#9d5e19"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#053d8b" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ffffff" BACKGROUND_COLOR="#1164b0" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffffff" BACKGROUND_COLOR="#298bc8" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#000000" BACKGROUND_COLOR="#3fb7db" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#051552" BACKGROUND_COLOR="#5cd5e8" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<node TEXT="" POSITION="bottom_or_right" ID="ID_77373083" CREATED="1746154261403" MODIFIED="1746154261403">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Scanning" POSITION="bottom_or_right" ID="ID_516958593" CREATED="1746153083461" MODIFIED="1746153085725">
+<node TEXT="Knows as lexing or lexical analysis" ID="ID_1857860323" CREATED="1746153110012" MODIFIED="1746153118521"/>
+<node TEXT="Chunk characters in tokens" ID="ID_1237263493" CREATED="1746153119522" MODIFIED="1746153147763">
+<node TEXT="Spaces and comments usually discarted" ID="ID_80944408" CREATED="1746153177453" MODIFIED="1746153252423" COLOR="#000000"/>
+</node>
+</node>
+<node TEXT="Parsing" POSITION="bottom_or_right" ID="ID_1395053936" CREATED="1746153086116" MODIFIED="1746153088959">
+<node TEXT="Build AST from tokens" ID="ID_461135203" CREATED="1746153312634" MODIFIED="1746153363428">
+<node TEXT="(AST) Abstract Syntax Tree" POSITION="bottom_or_right" ID="ID_969290049" CREATED="1746153710811" MODIFIED="1746153742204"/>
+<node TEXT="Syntax gets a grammar" POSITION="bottom_or_right" ID="ID_1855773974" CREATED="1746153415010" MODIFIED="1746153478686" COLOR="#000000"/>
+<node TEXT="Syntax error flow in this step" POSITION="bottom_or_right" ID="ID_1751340489" CREATED="1746153451336" MODIFIED="1746153698624"/>
+</node>
+</node>
+<node TEXT="Static analysis" POSITION="bottom_or_right" ID="ID_247771148" CREATED="1746153089172" MODIFIED="1746153096787">
+<node TEXT="Analysis of source code" ID="ID_778469920" CREATED="1746153906477" MODIFIED="1746153929264">
+<node TEXT="Binding or resolution" POSITION="bottom_or_right" ID="ID_1790208185" CREATED="1746153815109" MODIFIED="1746153835004">
+<node TEXT="Find definition of identifiers" ID="ID_1925876683" CREATED="1746153843512" MODIFIED="1746153876056"/>
+<node TEXT="Associate identifiers with declared entities" ID="ID_1446833478" CREATED="1746154029105" MODIFIED="1746154039978"/>
+</node>
+<node TEXT="Type checking" ID="ID_1016464136" CREATED="1746154063198" MODIFIED="1746154066904"/>
+<node TEXT="Control flow analysis" ID="ID_1904090618" CREATED="1746154074947" MODIFIED="1746154093367">
+<node TEXT="Identify code that will never execute" ID="ID_146970979" CREATED="1746154097600" MODIFIED="1746154105927"/>
+</node>
+</node>
+<node TEXT="Analysis needs to be stored" ID="ID_23599291" CREATED="1746154176981" MODIFIED="1746154188815">
+<node TEXT="As attributes on the syntax tree" ID="ID_1418318832" CREATED="1746154189024" MODIFIED="1746154202201">
+<node TEXT="Extra fields not initialized" ID="ID_1907180792" CREATED="1746154202633" MODIFIED="1746154208614"/>
+</node>
+<node TEXT="A table off to the side" ID="ID_1643824947" CREATED="1746154210082" MODIFIED="1746154216542">
+<node TEXT="Symbol table" ID="ID_1332882753" CREATED="1746154216745" MODIFIED="1746154235115">
+<node TEXT="Key - value" ID="ID_1434016528" CREATED="1746154235289" MODIFIED="1746154238547"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="" POSITION="bottom_or_right" ID="ID_919872886" CREATED="1746154261400" MODIFIED="1746154261403">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Front end" ID="ID_1142057817" CREATED="1746154261404" MODIFIED="1746154264679"/>
+</node>
+<node TEXT="Intermediate representation (IR)" POSITION="top_or_left" ID="ID_410041091" CREATED="1746154298624" MODIFIED="1746154319524">
+<node TEXT="Definition" ID="ID_59021766" CREATED="1746154320196" MODIFIED="1746154764808">
+<node TEXT="Internal code used by compilers" ID="ID_877448035" CREATED="1746154768400" MODIFIED="1746154781093"/>
+<node TEXT="Between source code and machine code" ID="ID_28703643" CREATED="1746154647043" MODIFIED="1746154682449"/>
+</node>
+<node TEXT="Purpose" ID="ID_1710273681" CREATED="1746154751465" MODIFIED="1746154794596">
+<node TEXT="Support multiple source languages" ID="ID_549613851" CREATED="1746154794980" MODIFIED="1746154825666"/>
+<node TEXT="Optimization" ID="ID_491072747" CREATED="1746154829300" MODIFIED="1746154837526"/>
+</node>
+</node>
+<node TEXT="Optimization" POSITION="top_or_left" ID="ID_809762456" CREATED="1746154306095" MODIFIED="1746154309895">
+<node TEXT="Constant folding" ID="ID_1063429348" CREATED="1746154873897" MODIFIED="1746154878301"/>
+</node>
+<node TEXT="Code generation" POSITION="top_or_left" ID="ID_602931104" CREATED="1746154916604" MODIFIED="1746154924402"/>
+</node>
+</map>
