c180a6232980c45571c4134427c7063b28d5efa7
Juan Zurita <juanzq10dev@gmail.com>
2026-05-24 16:41:13 -0400

    Add messages streaming

--- ai/agentic-ai/basic-agent-cli/main.py
+++ ai/agentic-ai/basic-agent-cli/main.py
@@ -12,28 +12,35 @@ def add_assistant_message(messages: list[MessageParam], text: str) -> list[Messa
 
 
 def chat(messages: list[MessageParam], client: Anthropic) -> str:
-    message = client.messages.create(
-        model="claude-haiku-4-5",
-        max_tokens=1000,
-        messages=messages,
-    )
-    return message.content[0].text
+    system_prompt = """
+    You are a very concise agent
+    """
 
+    params = {
+        "model": "claude-haiku-4-5",
+        "max_tokens": 1000,
+        "messages": messages,
+        "system_prompt": system_prompt,
+    }
 
-def loop(messages: list[MessageParam], client: Anthropic) -> None:
-    user_input = input("> ")
-    print(">", user_input)
-
-    messages = add_user_message(messages, user_input)
-    answer = chat(messages, client)
-    print(answer)
-
-    loop(add_assistant_message(messages, answer), client)
+    with client.messages.stream(**params) as stream:
+        for text in stream.text_stream:
+            print(text, end="")
+        print()
+        return stream.get_final_message().content[0].text
 
 
 def main() -> None:
     client = Anthropic(api_key=os.environ.get("ANTHROPIC_API_KEY"))
-    loop([], client)
+    messages: list[MessageParam] = []
+
+    while True:
+        user_input = input("> ")
+        print(">", user_input)
+
+        messages = add_user_message(messages, user_input)
+        answer = chat(messages, client)
+        messages = add_assistant_message(messages, answer)
 
 
 if __name__ == "__main__":
